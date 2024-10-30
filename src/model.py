Fixed causal masking for attention pattern

src/model.py
@@ -184,17 +184,19 @@ class CausalSelfAttention(nn.Module):
         self.dropout = config.dropout
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
         self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
-        if not self.flash:
-            print(
-                "WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0"
-            )
+        # if not self.flash:
+        #     print(
+        #         "WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0"
+        #     )
+        
+        print("Using causal masking")
             # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer(
-                "bias",
-                torch.tril(torch.ones(config.block_size, config.block_size)).view(
-                    1, 1, config.block_size, config.block_size
-                ),
-            )
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
 
     def forward(self, x):
         B, T, C = (
@@ -217,8 +219,8 @@ class CausalSelfAttention(nn.Module):
         if self.flash:
             # Calculate raw attention scores
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            # TODO: remember to add masking back in if using bias later
-            att_weights = F.softmax(att, dim=-1)  # Store attention weights
+            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+            att_weights = F.softmax(att, dim=-1)
 
             # efficient attention using Flash Attention CUDA kernels
             y = torch.nn.functional.scaled_dot_product_attention(