f1d2babaac01178f1221e1e6028e09739dfafd4f
Hannah Zhou <hzhou@college.harvard.edu>
2024-10-30 15:16:55 -0400

    Updated heatmap title

--- src/set_transformer_small.py
+++ src/set_transformer_small.py
@@ -221,7 +221,7 @@ def generate_heatmap(config, dataset_indices, use_labels=False):
     # Restore the model state dict
     checkpoint = torch.load(os.path.join(
         config.out_dir, config.filename), weights_only=False)
-    
+
     model.load_state_dict(checkpoint["model"])
     print("Loaded model")
 
@@ -237,16 +237,15 @@ def generate_heatmap(config, dataset_indices, use_labels=False):
         print("Got attention weights")
 
         labels = dataset[dataset_index].tolist()
-        
+
         if use_labels:
             labels = tokenizer.decode(labels)
             if "/" in labels:
-                filename_set = "two_sets"
+                number_set = "two"
             elif "*" in labels:
-                filename_set = "no_sets"
+                number_set = "zero"
             else:
-                filename_set = "one_set"
-
+                number_set = "one"
 
         print("labels: ", labels)
 
@@ -257,8 +256,8 @@ def generate_heatmap(config, dataset_indices, use_labels=False):
                 plot_attention_heatmap(
                     attention_weights[layer][0][head],
                     labels,
-                    title=f"Attention Weights: Layer {layer}, Head {head}",
-                    savefig=f"causal_masking_layers_2_heads_4/attention_heatmap_{filename_set}_index_{dataset_index}_layer_{layer}_head_{head}.png")
+                    title=f"{number_set.capitalize()} Sets. Attention Weights: Layer {layer}, Head {head}",
+                    savefig=f"causal_masking_layers_2_heads_4/attention_heatmap_sets_{number_set}_index_{dataset_index}_layer_{layer}_head_{head}.png")
 
 
 if __name__ == "__main__":
@@ -280,6 +279,4 @@ if __name__ == "__main__":
     # generate_heatmap(GPTConfig(), [0, 1, 4], use_labels=True)
     generate_heatmap(GPTConfig24(), [0, 1, 4], use_labels=True)
 
-
     # dataset = initialize_datasets(GPTConfig(), save_dataset=False, save_tokenizer_path = '/n/holylabs/LABS/wattenberg_lab/Lab/hannahgz_tmp/balanced_set_dataset_random_tokenizer.pkl')
-
