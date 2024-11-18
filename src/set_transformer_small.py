ce01a98b03c12161622ea37dc3e7fd1adfaab5d6
Hannah Zhou <hzhou@college.harvard.edu>
2024-11-18 15:50:19 -0500

    layers 2 and 3 should be fixed

--- src/set_transformer_small.py
+++ src/set_transformer_small.py
@@ -295,34 +295,34 @@ if __name__ == "__main__":
     # dataset_name = "balanced_set_dataset_random"
     # get_raw_input_embeddings(GPTConfig44, dataset_name, capture_layer=0)
 
-    # dataset_name = "balanced_set_dataset_random"
-    # for layer in range(1,4):
-    #     embeddings_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/input_embeddings.pt"
-    #     mapped_attributes_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/mapped_target_attributes.pt"
-
-    #     X = torch.load(embeddings_path)
-    #     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    #     y = torch.load(mapped_attributes_path).to(device)
-
-    #     run_classify(X, y, model_name=f"{dataset_name}_layer{layer}", input_dim=64, output_dim=5)
-    #     # run_classify(X, y, model_name=f"{dataset_name}_layer{layer}", input_dim=64, output_dim=5, model_type="mlp")
-
-    layer = 0
     dataset_name = "balanced_set_dataset_random"
-    embeddings_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/input_embeddings.pt"
-    mapped_attributes_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/mapped_target_attributes.pt"
-    X = torch.load(embeddings_path)
-    y = torch.load(mapped_attributes_path)
+    for layer in range(2,4):
+        embeddings_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/input_embeddings.pt"
+        mapped_attributes_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/mapped_target_attributes.pt"
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    y = y.to(device)
+        X = torch.load(embeddings_path)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        y = torch.load(mapped_attributes_path).to(device)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        run_classify(X, y, model_name=f"{dataset_name}_layer{layer}", input_dim=64, output_dim=5)
+        # run_classify(X, y, model_name=f"{dataset_name}_layer{layer}", input_dim=64, output_dim=5, model_type="mlp")
 
-    model = LinearModel(input_dim=64, output_dim=5).to(device)
+    # layer = 0
+    # dataset_name = "balanced_set_dataset_random"
+    # embeddings_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/input_embeddings.pt"
+    # mapped_attributes_path = f"{PATH_PREFIX}/classify/{dataset_name}/layer{layer}/mapped_target_attributes.pt"
+    # X = torch.load(embeddings_path)
+    # y = torch.load(mapped_attributes_path)
 
-    evaluate_model(model, X_train, y_train, model_name=f"{dataset_name}_layer{layer}_linear")
-    evaluate_model(model, X_test, y_test, model_name=f"{dataset_name}_layer{layer}_linear")
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # y = y.to(device)
+
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # model = LinearModel(input_dim=64, output_dim=5).to(device)
+
+    # evaluate_model(model, X_train, y_train, model_name=f"{dataset_name}_layer{layer}_linear")
+    # evaluate_model(model, X_test, y_test, model_name=f"{dataset_name}_layer{layer}_linear")
 
     # embeddings_path = f"{PATH_PREFIX}/classify/full_combined_input_embeddings.pt"
     # mapped_attributes_path = f"{PATH_PREFIX}/classify/full_mapped_target_attributes.pt"
