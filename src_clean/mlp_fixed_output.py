865076ac1af56600ec02a5ac07445e4861b8a02c
Hannah Zhou <hzhou@college.harvard.edu>
2025-03-13 17:37:21 -0400

    checking norms

--- src_clean/mlp_fixed_output.py
+++ src_clean/mlp_fixed_output.py
@@ -15,54 +15,62 @@ from mlp_fixed import plot_overlap_histograms
 device = "cuda" if torch.cuda.is_available() else "cpu"
 PATH_PREFIX = '/n/holylabs/LABS/wattenberg_lab/Lab/hannahgz_tmp'
 
+def analyze_mlp_weights(mlp_weights):
+    for layer_name, layer_weights in mlp_weights.items():
+        for weight_name, weights in layer_weights.items():
+            col_norms = np.linalg.norm(weights, axis=0)
+            breakpoint()
 
 if __name__ == "__main__":
 
-    config = GPTConfig44_Complete()
-    # Load the checkpoint
-    checkpoint = torch.load(config.filename, weights_only=False)
+    mlp_weights = torch.load(f"{PATH_PREFIX}/mlp_triples_card_randomization_tuple_randomization_layers_4_heads_4.pt")
+    analyze_mlp_weights(mlp_weights)
 
-    # Create the model architecture
-    model = GPT(config).to(device)
+    # config = GPTConfig44_Complete()
+    # # Load the checkpoint
+    # checkpoint = torch.load(config.filename, weights_only=False)
 
-    # Load the weights
-    model.load_state_dict(checkpoint['model'])
-    model.eval()  # Set to evaluation mode
+    # # Create the model architecture
+    # model = GPT(config).to(device)
 
-    dataset_path = f"{PATH_PREFIX}/base_card_randomization_tuple_randomization_dataset.pth"
-    dataset = torch.load(dataset_path)
-    _, val_loader = initialize_loaders(config, dataset)
+    # # Load the weights
+    # model.load_state_dict(checkpoint['model'])
+    # model.eval()  # Set to evaluation mode
 
-    neuron_indices = [5, 13, 20, 36, 60]
-    for curr_layer in [3]:
-        print(f"Analyzing layer {curr_layer}")
-        neuron_activations = analyze_mlp_neurons(
-            model=model,
-            data_loader=val_loader,
-            layer_idx=curr_layer,
-            neuron_indices=neuron_indices,
-            mode='output',
-            position_slice=slice(-8,None))
+    # dataset_path = f"{PATH_PREFIX}/base_card_randomization_tuple_randomization_dataset.pth"
+    # dataset = torch.load(dataset_path)
+    # _, val_loader = initialize_loaders(config, dataset)
 
-        output_dir = f"{PATH_PREFIX}/data/mlp_fixed/output/layer{curr_layer}"
-        # # Make sure the output directory exists
-        os.makedirs(output_dir, exist_ok=True)
+    # neuron_indices = [5, 13, 20, 36, 60]
+    # for curr_layer in [3]:
+    #     print(f"Analyzing layer {curr_layer}")
+    #     neuron_activations = analyze_mlp_neurons(
+    #         model=model,
+    #         data_loader=val_loader,
+    #         layer_idx=curr_layer,
+    #         neuron_indices=neuron_indices,
+    #         mode='output',
+    #         position_slice=slice(-8,None))
 
-        pkl_filename = os.path.join(
-            output_dir, f"neuron_activations_layer{curr_layer}.pkl")
+    #     output_dir = f"{PATH_PREFIX}/data/mlp_fixed/output/layer{curr_layer}"
+    #     # # Make sure the output directory exists
+    #     os.makedirs(output_dir, exist_ok=True)
 
-        # Save the activations to a pickle file
-        with open(pkl_filename, 'wb') as f:
-            pickle.dump(neuron_activations, f)
+    #     pkl_filename = os.path.join(
+    #         output_dir, f"neuron_activations_layer{curr_layer}.pkl")
 
-        print("Plotting histograms for interesting output neurons")
-        output_dir = f"results/mlp_fixed/peaks/output/layer{curr_layer}"
-        os.makedirs(output_dir, exist_ok=True)
+    #     # Save the activations to a pickle file
+    #     with open(pkl_filename, 'wb') as f:
+    #         pickle.dump(neuron_activations, f)
+
+    #     print("Plotting histograms for interesting output neurons")
+    #     output_dir = f"results/mlp_fixed/peaks/output/layer{curr_layer}"
+    #     os.makedirs(output_dir, exist_ok=True)
     
-        fig = plot_overlap_histograms(
-            neuron_activations = neuron_activations,
-            target_neurons = neuron_indices,
-            num_bins = 50,
-        )
+    #     fig = plot_overlap_histograms(
+    #         neuron_activations = neuron_activations,
+    #         target_neurons = neuron_indices,
+    #         num_bins = 50,
+    #     )
 
-        plt.savefig(f"{output_dir}/consolidated_overlap_heatmap_interesting_from_heatmap.png")
+    #     plt.savefig(f"{output_dir}/consolidated_overlap_heatmap_interesting_from_heatmap.png")
