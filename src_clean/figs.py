7286364268b4da776f6ef22827e2c57ff3de98eb
Hannah Zhou <hzhou@college.harvard.edu>
2025-03-21 16:58:50 -0700

    fixed fig ranges

--- src_clean/figs.py
+++ src_clean/figs.py
@@ -865,6 +865,8 @@ def plot_consolidated_same_card(layers, tokenizer_path, loss_range=[0, 0.8], acc
             # axes_train_loss[layer_idx].set_ylim(loss_range[0], loss_range[1])
             # axes_train_loss[layer_idx].set_yscale('log')
             axes_train_loss[layer_idx].ticklabel_format(useOffset=False, style='plain')
+            if layer_idx != 0:
+                axes_train_loss[layer_idx].set_ylim(-0.001, 0.03)
             axes_train_loss[layer_idx].tick_params(labelsize=annot_font_size)
 
             # Plot validation loss
@@ -880,6 +882,8 @@ def plot_consolidated_same_card(layers, tokenizer_path, loss_range=[0, 0.8], acc
             # axes_val_loss[layer_idx].set_ylim(loss_range[0], loss_range[1])
             # axes_val_loss[layer_idx].set_yscale('log')
             axes_val_loss[layer_idx].ticklabel_format(useOffset=False, style='plain')
+            if layer_idx != 0:
+                axes_val_loss[layer_idx].set_ylim(-0.001, 0.013)
             axes_val_loss[layer_idx].tick_params(labelsize=annot_font_size)
 
             # Plot training accuracy
@@ -895,6 +899,8 @@ def plot_consolidated_same_card(layers, tokenizer_path, loss_range=[0, 0.8], acc
             # axes_train_acc[layer_idx].set_ylim(acc_range[0], acc_range[1])
             # axes_train_acc[layer_idx].set_yscale('log')
             axes_train_acc[layer_idx].ticklabel_format(useOffset=False, style='plain')
+            if layer_idx != 0:
+                axes_train_acc[layer_idx].set_ylim(0.992, 1.01)
             axes_train_acc[layer_idx].tick_params(labelsize=annot_font_size)
 
             # Plot validation accuracy
@@ -910,6 +916,8 @@ def plot_consolidated_same_card(layers, tokenizer_path, loss_range=[0, 0.8], acc
             # axes_val_acc[layer_idx].set_ylim(acc_range[0], acc_range[1])
             # axes_val_acc[layer_idx].set_yscale('log')
             axes_val_acc[layer_idx].ticklabel_format(useOffset=False, style='plain')
+            if layer_idx != 0:
+                axes_val_acc[layer_idx].set_ylim(0.996, 1.01)
             axes_val_acc[layer_idx].tick_params(labelsize=annot_font_size)
 
         # Only add legend to the last subplot
